fe95495e6ef250cb1239d7f1d1e2b3e9f262243f
Mszyd <mszydlowski@student.agh.edu.pl>
2025-11-13 17:53:19 +0100

    Zad 3: du -sh /*/ 2>/dev/null|sort -h -r|tail -5

---
+++ scripts/report.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+kat="${1:-}" #utworzenie zmiennej dla  kataloga wejściowego
+if [ "$kat" = "-n" ]; then
+echo "program patrzy czy folder istnieje, jeśli nie to wypisuję bład"
+echo "program przeszukuje podany katalog i wypisuję 5 największych podkatalogów"
+exit 0   
+fi
+if [ ! -d "$kat" ]; then #warunek który sprawdza czy katalog istnieje
+echo "katalog nie istnieje" #wypisanie  o nie istneniu katalogu
+exit 1 # kod kończy program z wyjściem 1 co znaczy ż coś poszło nie tak, tu katalog nie istnieje
+fi  
+date +"%F %T" #wypisanie daty w podanym formacie
+du -sh "$kat"/*/ 2>/dev/null|sort -h -r |tail -5 #program  wypisuje podkatalogi folderu danego na wejściu, sortuje je i wypisuję 5 największych
